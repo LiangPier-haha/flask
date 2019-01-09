1beb014080295ae80ea769e75ace4693da9aab1e
“LiangPier-haha” <17853246116@163.com>
2019-01-09 12:00:04 +0800

    property

---
+++ suanfa/property.py
@@ -0,0 +1,42 @@
+import time
+from threading import Thread
+
+
+def func(f):
+    def func2(*arg,**keyword):
+        g = f(*arg,**keyword)
+        gg = next(g)
+        def inner(ggg):
+            ggg = next(gg)
+            try:
+                g.send(ggg)
+            except:
+                pass
+        thread = Thread(target=inner,args=[gg,])
+        thread.start()
+
+    return func2
+def follow_to():
+    print('start follow_to')
+    yield 'haha'
+    print('end follow_to')
+@func
+def rep_a():
+    print('start rep_a')
+
+    s = yield follow_to()
+    time.sleep(5)
+    print(s)
+    print('end rep_a')
+
+def rep_b():
+    print('start rep_b')
+    time.sleep(2)
+    print('end rep_b')
+
+
+if __name__=='__main__':
+    rep_a()
+    rep_b()
+    while 1:
+        pass
